a37bf53ffcf15009f8773369e4345f1b1e045e9f
dilipsdil <dilipsdil@gmail.com>
2013-03-27 12:57:17 +0300

    Create to-sort.js

---
+++ js/to-sort.js
@@ -0,0 +1,56 @@
+<script type="text/javascript" >
+function showStuff(id) {
+if(id=='main_table_income'){
+    document.getElementById('main_table_income').style.display = 'block';
+  document.getElementById('main_table_ctl').style.display = 'none';
+	document.getElementById('main_table_non').style.display = 'none';
+	//document.getElementById('switch').innerHTML = '<a href="#" onclick="showStuff(\'main_table_non\'); return false;">Switch Report</a>';
+	}
+	else if(id=='main_table_ctl'){
+	  document.getElementById('main_table_income').style.display = 'none';
+    document.getElementById('main_table_ctl').style.display = 'block';
+	document.getElementById('main_table_non').style.display = 'none';
+	//document.getElementById('switch').innerHTML = '<a href="#" onclick="showStuff(\'main_table_non\'); return false;">Switch Report</a>';
+	}
+	else{
+	document.getElementById('main_table_income').style.display = 'none';
+	document.getElementById('main_table_ctl').style.display = 'none';
+	document.getElementById('main_table_non').style.display = 'block';
+	//document.getElementById('switch').innerHTML = '<a href="#" onclick="showStuff(\'main_table_ctl\'); return false;">Switch Report</a>';
+	}
+}
+
+function SelectContent (el) {
+
+
+var elemToSelect = document.getElementById (el);
+
+        if (window.getSelection) {  // all browsers, except IE before version 9
+            var selection = window.getSelection ();
+            var rangeToSelect = document.createRange ();
+            rangeToSelect.selectNodeContents (elemToSelect);
+
+            selection.removeAllRanges ();
+            selection.addRange (rangeToSelect);
+
+
+
+        }
+
+    else       // Internet Explorer before version 9
+          if (document.body.createTextRange) {    // Internet Explorer
+                var rangeToSelect = document.body.createTextRange ();
+                rangeToSelect.moveToElementText (elemToSelect);
+                rangeToSelect.select ();
+
+        }
+
+  else if (document.createRange && window.getSelection) {         
+          range = document.createRange();             
+          range.selectNodeContents(el);             
+    sel = window.getSelection();     
+                  sel.removeAllRanges();             
+    sel.addRange(range);              
+ }  
+}
+</script>
