4c90e6b28dc8d1b7d145223fb96885a9d68ee384
dilipsdil <dilipsdil@gmail.com>
2013-04-10 15:02:30 +0300

    Update cool.sql

--- sql/cool.sql
+++ sql/cool.sql
@@ -1,7 +1,7 @@
 --useful for getting laste updated time on a table, 
-SELECT  SCN_TO_TIMESTAMP(MAX(ora_rowscn)) from TABLE_NAME
+select  SCN_TO_TIMESTAMP(max(ORA_ROWSCN)) from TABLE_NAME
 
 
 --if the table is truncated or does not have any data the follwoing is adjusted to prevent errors
 
-SELECT  SCN_TO_TIMESTAMP(nvl(MAX(ora_rowscn),TIMESTAMP_TO_SCN(sysdate))) from TABLE_NAME;
+select  SCN_TO_TIMESTAMP(nvl(max(ORA_ROWSCN),TIMESTAMP_TO_SCN(SYSDATE))) from TABLE_NAME;
